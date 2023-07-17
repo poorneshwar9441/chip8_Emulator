f2f271eeb26533f9cd1997034691014267988dae
poorneshwar9441 <poorneshchaganti@gmail.com>
2023-07-17 18:08:57 +0530

    refactored

--- keyboard.py
+++ keyboard.py
@@ -2,25 +2,15 @@
 
 class Keyboard(object):
 	def __init__(self):
-		self.dic = {0 : False,1:False,
-		 2:False,
-		 3:False,
-		 4:False,
-		 5:False,
-		 6:False,
-		 7:False,
-		 8:False,
-		 9:False,
-		 10:False,
-		 11:False,
-		 12:False,
-		 13:False,
-		 14:False,
-		 15:False
+		self.dic = {
+		 
 		} #keyboard dictnory 
 
 		self.mapping = { 
 		} 
+
+	for i in range(16):
+		self.dic[i] = False
 
 
 	def __str__(self):
